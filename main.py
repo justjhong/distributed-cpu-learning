3d9e1a2d337914be3443a1546c53388f51336735
Justin Hong <jjhong922@gmail.com>
2019-05-03 23:40:45 +0000

    constant init batch size

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import subprocess
 
 cmds = [
-        ["mpirun", "-np", "16", "-H", "localhost:16", "--bind-to", "hwthread", "python3", "horovod_train.py", "--num-cores", "16"],
-        ["mpirun", "-np", "8", "-H", "localhost:8", "--bind-to", "hwthread", "python3", "horovod_train.py", "--num-cores", "8"],
-        ["mpirun", "-np", "4", "-H", "localhost:4", "--bind-to", "hwthread","python3", "horovod_train.py",  "--num-cores", "4"],
-        ["mpirun", "-np", "16", "-H", "localhost:16", "--bind-to", "hwthread", "python3", "horovod_train.py", "--num-cores", "16", "--comm-interval", "5"],
-        ["mpirun", "-np", "16", "-H", "localhost:16", "--bind-to", "hwthread", "python3", "horovod_train.py", "--num-cores", "16", "--comm-interval", "10"],
+        # ["mpirun", "-np", "16", "-H", "localhost:16", "--bind-to", "hwthread", "python3", "horovod_train.py", "--num-cores", "16"],
+        # ["mpirun", "-np", "8", "-H", "localhost:8", "--bind-to", "hwthread", "python3", "horovod_train.py", "--num-cores", "8"],
+        # ["mpirun", "-np", "4", "-H", "localhost:4", "--bind-to", "hwthread","python3", "horovod_train.py",  "--num-cores", "4"],
+        # ["mpirun", "-np", "16", "-H", "localhost:16", "--bind-to", "hwthread", "python3", "horovod_train.py", "--num-cores", "16", "--comm-interval", "5"],
+        # ["mpirun", "-np", "16", "-H", "localhost:16", "--bind-to", "hwthread", "python3", "horovod_train.py", "--num-cores", "16", "--comm-interval", "10"],
         ["mpirun", "-np", "16", "-H", "localhost:16", "--bind-to", "hwthread",  "python3", "hessian_horovod_train.py", "--num-cores", "16"],
         ["mpirun", "-np", "16", "-H", "localhost:16", "--bind-to", "hwthread","python3", "hessian_horovod_train.py",  "--num-cores", "16", "--batch-mult", "2"],
         ["mpirun", "-np", "16", "-H", "localhost:16", "--bind-to", "hwthread", "python3", "hessian_horovod_train.py", "--num-cores", "16", "--batch-mult", "4"],
@@ -14,7 +14,7 @@ cmds = [
         ["mpirun", "-np", "4", "-H", "localhost:4", "--bind-to", "hwthread", "python3", "hessian_horovod_train.py", "--num-cores", "4"],
         ["mpirun", "-np", "16", "-H", "localhost:16", "--bind-to", "hwthread", "python3", "hessian_horovod_train.py", "--num-cores", "16", "--comm-interval", "5"],
         ["mpirun", "-np", "16", "-H", "localhost:16", "--bind-to", "hwthread", "python3", "hessian_horovod_train.py", "--num-cores", "16", "--comm-interval", "10"],
-        ["mpirun", "-np", "16", "-H", "localhost:16", "--bind-to", "hwthread", "python3", "hessian_horovod_train.py", "--num-cores", "16", "--eig_comm", "0"],
+        ["mpirun", "-np", "16", "-H", "localhost:16", "--bind-to", "hwthread", "python3", "hessian_horovod_train.py", "--num-cores", "16", "--eig-comm", "0"],
         ]
 
 for cmd in cmds:
