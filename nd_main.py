8ec22d5d8316a1e84a915200d4977a0247eb9035
Justin Hong <jjhong922@gmail.com>
2019-05-04 19:42:20 +0000

    add duration batch change and new results - not good

--- nd_main.py
+++ nd_main.py
@@ -2,7 +2,9 @@ import subprocess
 
 cmds = [
         # ["python3", "nd_horovod_train.py", "--batch-size", "128"],
-        ["python3", "nd_hessian_horovod_train.py", "--init-batch-size", "128"],
+        # ["python3", "nd_hessian_horovod_train.py", "--init-batch-size", "128"],
+        ["python3", "nd_hessian_horovod_train.py", "--init-batch-size", "128", "--lr", "0.01"],
+        ["python3", "nd_horovod_train.py", "--batch-size", "128", "--lr", "0.01"],
         ]
 
 for cmd in cmds:
